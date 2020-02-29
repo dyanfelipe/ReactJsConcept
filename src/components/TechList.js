98a835b1ac18e4881cc6065fd4be570accb584a8
dyanfelipe <dyansilva@outlook.com>
2020-02-29 20:40:13 -0300

    Imutabilidade

---
+++ src/components/TechList.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+class TechList extends Component {
+  state = {
+    newTech: '',
+    techs: ['Node.js', 'Flask', 'Python', 'ReactJS', 'React Native']
+  };
+
+  handleInputChange = e => {
+    this.setState({ newTech: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: ''
+    });
+  };
+
+  render() {
+    console.log(this.state);
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          <h1>{this.state.newTech}</h1>
+          {this.state.techs.map(tech => (
+            <li key={tech}>{tech}</li>
+          ))}
+        </ul>
+        <input
+          type="text"
+          onChange={this.handleInputChange}
+          value={this.state.newTech}
+        />
+        <button type="submit">Enviar</button>
+      </form>
+    );
+  }
+}
+
+export default TechList;
